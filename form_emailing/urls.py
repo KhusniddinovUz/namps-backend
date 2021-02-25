7841f4a4efcc253158c29da68828316d9f32a9df
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-02-25 20:46:41 +0500

    Send email from view

--- form_emailing/urls.py
+++ form_emailing/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import SendEmail
 
 urlpatterns = [
-    path('', SendEmail.as_view),
+    path('', SendEmail.as_view(), ),
 ]
--- form_emailing/views.py
+++ form_emailing/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import HttpResponse
 from rest_framework.views import APIView
+from django.core.mail import send_mail
 
 
 class SendEmail(APIView):
     def post(self, request, *args, **kwargs):
         email = request.data['email']
+        message = request.data['message']
+        send_mail('Message for Nam PM', f'From {email} : {message}', None, ['khusniddinovuz@gmail.com'])
         return HttpResponse('Cool')
