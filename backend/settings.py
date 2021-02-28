0f7540f40b6531d069263954e9528eabd0025c29
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-02-28 18:00:18 +0500

    Allow localhost to send request

--- backend/settings.py
+++ backend/settings.py
@@ -41,12 +41,19 @@ INSTALLED_APPS = [
 
     # third party packages
     'rest_framework',
+    'corsheaders',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
